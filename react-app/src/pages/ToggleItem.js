503c94b29533c260ce624002b3977dcae784e9e4
sylee <linggo01@naver.com>
2023-08-24 17:59:27 +0900

    33

--- react-app/src/pages/ToggleItem.js
+++ react-app/src/pages/ToggleItem.js
@@ -1,16 +1,31 @@
 import { useState } from "react";
 
 function ToggleItem({title, content, active}){
+
   const [isActive, setIsActive] = useState(active);
+
+  // const getSiblings = elm => elm && elm.parentNode && [...elm.parentNode.children].filter(node => node != elm);
+  // getSiblings(element);
 
   const toggleSlide = (e) => {
     e.preventDefault();
+
     setIsActive(!isActive); //true, false 반대로
-    //setIsActive(isActive => !isActive);
+    const el = e.currentTarget.parentNode.parentNode.childNodes
+    const active = e.currentTarget.parentNode.dataset.state;
+
+    el.forEach(element => {
+      if(element.dataset.state == true) {
+        console.log('11')
+      }
+    });
+
+    //모든 el 중에 state가 true 인게 있으면 
+    // 그 애들만 ㄴㅅ
   }
   return (
-    <div className={`toggleItem ${isActive ? "on" : ""}`}>
-      <button className="tgBtn" onClick={toggleSlide}>{title}</button>
+    <div className={`toggleItem ${isActive ? "on" : ""}`} data-state={isActive}>
+      <button className="tgBtn" onClick={toggleSlide} >{title}</button>
       <div className="tgCont">{content}</div>
     </div>
   );
