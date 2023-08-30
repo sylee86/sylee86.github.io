f24838af1d2baa5221dc6d4482a267decff9d2ff
sylee <linggo01@naver.com>
2023-08-30 17:23:02 +0900

    toggle 다른토글 닫힘처리

--- react-app/src/pages/Toggle.js
+++ react-app/src/pages/Toggle.js
@@ -1,21 +1,44 @@
 import AppHeader from './../components/AppHeader.js';
-import ToggleItem from "./ToggleItem"
+import { useState } from "react";
 
 function Toggle(){
-  const datas = [
-    {id:1, title:"토글버튼1", content:"내용입니다1"},
-    {id:2, title:"토글버튼2", content:"내용입니다2"},
-    {id:3, title:"토글버튼3", content:"내용입니다3"},
-    {id:4, title:"토글버튼4", content:"내용입니다4"},
-    {id:5, title:"토글버튼5", content:"내용입니다5"},
-  ]
+  const [datas, setDatas] = useState([
+    {id:1, title:"토글버튼1", content:"내용입니다1", select:false},
+    {id:2, title:"토글버튼2", content:"내용입니다2", select:false},
+    {id:3, title:"토글버튼3", content:"내용입니다3", select:false},
+    {id:4, title:"토글버튼4", content:"내용입니다4", select:false},
+    {id:5, title:"토글버튼5", content:"내용입니다5", select:false},
+  ]);
 
+
+  const toggleSlide = (e) => {
+    e.preventDefault();
+
+    const idx = Number(e.currentTarget.parentNode.dataset.index);
+    const newDatas = datas.map(k => {
+      if (k.id === idx) {
+        return {
+          ...k,
+          select: true,
+        };
+      } else {
+        return {
+          ...k,
+          select: false,
+        };
+      }
+    });
+    setDatas(newDatas);
+  }
   return (
     <div>
       <AppHeader title="Toggle" />
       <div className="ToggleWrap">
         {datas.map(data => 
-          <ToggleItem key={data.id} title={data.title} content={data.content} active={false} />
+          <div key={data.id} data-index={data.id} className={`toggleItem ${data.select == true ? "on" : ""}`}>
+            <button className="tgBtn" onClick={toggleSlide} >{data.title}</button>
+            <div className="tgCont">{data.content}</div>
+          </div>
         )}
       </div>
     </div>
